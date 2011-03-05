a65e50a828d387492382cb8d746b60dff7013494
lrf2123 <lrf2123@columbia.edu>
2011-03-05 13:33:23 -0500

    updated readlock and readunlock to courtois et al

--- HW3/xv6/rwlock.c
+++ HW3/xv6/rwlock.c
@@ -36,21 +36,25 @@ destroyrwlock(struct rwlock *m)
 void
 readlock(struct rwlock *m)
 {
-	acquire (m->guard);
-	++ m->nreader;
-	if(m->nreader == 1) // first reader 
-		acquire(m->lock); 
-	release(m->guard);
+	acquire (m->mutex3);
+		acquire (m->r);
+			acquire(m->mutex1);
+				++ m->nreader;
+				if(m->nreader == 1) 
+					acquire(m->w);
+			release(m->mutex1);
+		release (m->r);
+	release (m->mutex3);
 }
 
 void
 readunlock(struct rwlock *m)
 {
-	acquire(m->guard);
-	-- m->nreader;
-	if(m->nreader == 0) // last reader 
-		release(m->lock);
-	release(m->guard);
+	acquire (m->mutex1);
+		-- m->readcount;
+		if (m->readcount == 0)
+			release (m->w);
+	release (m->mutex1);
 }
 
 void
